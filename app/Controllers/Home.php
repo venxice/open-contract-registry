872b46283fc6324abb2aa24f94eb1ab8a852aa09
venxice <venicedon17@gmail.com>
2026-08-25 10:12:34 +0800

    Convert HTML views to PHP, fix database config and routing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,13 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        $file = FCPATH . 'home_spa.html';
+        if (file_exists($file)) {
+            return $this->response->setBody(file_get_contents($file))
+                ->setHeader('Content-Type', 'text/html');
+        }
         return view('welcome_message');
     }
 }
